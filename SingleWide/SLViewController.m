6fd12f8b25ba592b978e5483aeb32e9d123644a3
Mark Stultz <mstultz@gmail.com>
2014-02-11 21:13:11 -0600

    Initial Xcode project

---
+++ SingleWide/SLViewController.m
@@ -0,0 +1,29 @@
+//
+//  SLViewController.m
+//  SingleWide
+//
+//  Created by Mark Stultz on 2/11/14.
+//  Copyright (c) 2014 Summer League. All rights reserved.
+//
+
+#import "SLViewController.h"
+
+@interface SLViewController ()
+
+@end
+
+@implementation SLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SingleWide/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SingleWide
+//
+//  Created by Mark Stultz on 2/11/14.
+//  Copyright (c) 2014 Summer League. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLAppDelegate class]));
+	}
+}
